33bc7041f13a9f04b3fd5fb47a71191223334f57
Proton# <protoncoding@outlook.com>
2026-02-26 21:42:11 -0600

    Create template for teams.js with user and link details
    Add template structure for teams including user roles and links.

---
+++ template for teams.js
@@ -0,0 +1,19 @@
+// This has all the templates to add and remove links or people.
+//The first one is the jobs:
+
+{
+  username: "FriendUsername",
+  role: "Positon",
+  description: "What they do",
+  about: "Their Bio",
+  emoji: "Emoji for the positon"
+},
+
+//This one is for links
+discord: "https://discord.gg/yourlink",
+  //This one is better to break down
+
+  //discord:
+  //This means that the socail link is discord
+  //"https://discord.gg/yourlink",
+  //This is the link and pressing the button leads there.
